2d7a8e53bbb9a0c28b2e071c17a9b9b229acd9da
Carlos Dela Cruz <cdelacruzzin18@gmail.com>
2023-04-27 21:51:27 -0400

    fixed function errors

--- script.js
+++ script.js
@@ -20,62 +20,6 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-
-function generatePassword() {
-    let criteria = confirm("Here is a list of criteria to include in the password: \n -Length\n-Character Types\n\nPress OK to confim");
-
-    //stores the prompt input into 'passwordLength
-    let passwordLength = prompt("Enter a number between 8 and 128.");
-
-    //passes the value of the input to the passLength function
-    passwordLength = passLength(passwordLength);
-    console.log(passwordLength);
-
-
-    confirm("Here is a list of possible character types to include in your password:\nlowercase\nuppercase\nnumeric\nspecial characters")
-
-
-    //prompts wheter to include lowercase, uppercase, numbers, or special characters in password
-    var lowercase = confirm("Do you want to include lowercases?");
-
-    var uppercase = confirm("Do you want to include uppercase?");
-
-    var numeric = confirm("Do you want to include numeric?");
-
-    var specialCharacters = confirm("Do you want to include special characters?");
-
-
-    //computes the length of the number of criteria selected.\
-    //there are 4 criteria, it will decrease by 1 if 'cancel' button is clicked. 
-    //an array 'criteria array' is created, and if true, the array's length will increase, and the the corresponding function call will be added on the array index
-    var criteriaCount = 4;
-    var criteriaArray = [];
-
-    if (lowercase == false) {
-        criteriaCount--;
-    } else {
-        criteriaArray.push(lowercase());
-    }
-
-    if (uppercase == false) {
-        criteriaCount--;
-    } else {
-        criteriaArray.push(uppercase());
-    }
-    if (numeric == false) {
-        criteriaCount--;
-    } else {
-        criteriaArray.push(randomInt());
-    }
-
-    if (specialCharacters == false) {
-        criteriaCount--;
-    } else {
-        criteriaArray.push(specialChar());
-    }
-
-}
-
 function passLength(pass) {
     //checks whether password length is within the parameters, and is a number, and loops the prompt if it isn't
     //after it passes the loops conditions, the input will be parsed into a number. 
@@ -120,3 +64,73 @@ function lowercase() {
 function uppercase() {
     return lowercase().toUpperCase();
 }
+
+
+
+
+
+
+
+
+
+
+
+function generatePassword() {
+    let criteria = confirm("Here is a list of criteria to include in the password: \n -Length\n-Character Types\n\nPress OK to confim");
+
+    //stores the prompt input into 'passwordLength
+    let passwordLength = prompt("Enter a number between 8 and 128.");
+
+    //passes the value of the input to the passLength function
+    passwordLength = passLength(passwordLength);
+    console.log(passwordLength);
+
+
+    confirm("Here is a list of possible character types to include in your password:\nlowercase\nuppercase\nnumeric\nspecial characters")
+
+
+    //prompts wheter to include lowercase, uppercase, numbers, or special characters in password
+    var lowercasePrompt = confirm("Do you want to include lowercases?");
+
+    var uppercasePrompt = confirm("Do you want to include uppercase?");
+
+    var numericPrompt = confirm("Do you want to include numeric?");
+
+    var specialCharactersPrompt = confirm("Do you want to include special characters?");
+
+
+    //computes the length of the number of criteria selected.\
+    //there are 4 criteria, it will decrease by 1 if 'cancel' button is clicked. 
+    //an array 'criteria array' is created, and if true, the array's length will increase, and the the corresponding function call will be added on the array index
+    var criteriaCount = 4;
+    var criteriaArray = [];
+
+    if (lowercasePrompt == false) {
+        criteriaCount--;
+    } else {
+        criteriaArray.push(lowercase());
+    }
+
+    if (uppercasePrompt == false) {
+        criteriaCount--;
+    } else {
+        criteriaArray.push(uppercase());
+    }
+    if (numericPrompt == false) {
+        criteriaCount--;
+    } else {
+        criteriaArray.push(randomInt());
+    }
+
+    if (specialCharactersPrompt == false) {
+        criteriaCount--;
+    } else {
+        criteriaArray.push(specialChar());
+    }
+
+    for (var a = 0; a < criteriaArray.length; a++) {
+        console.log(criteriaArray[a]);
+
+    }
+}
+
