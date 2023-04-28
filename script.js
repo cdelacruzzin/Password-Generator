ee583ed284cb11b7461e813e6daef2f532b45cff
Carlos Dela Cruz <cdelacruzzin18@gmail.com>
2023-04-27 22:08:08 -0400

    made another function called 'generateCriteria' to do computation there

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-    var password = generatePassword();
+     var password = prompts();
     var passwordText = document.querySelector("#password");
 
     passwordText.value = password;
@@ -75,7 +75,7 @@ function uppercase() {
 
 
 
-function generatePassword() {
+function prompts() {
     let criteria = confirm("Here is a list of criteria to include in the password: \n -Length\n-Character Types\n\nPress OK to confim");
 
     //stores the prompt input into 'passwordLength
@@ -98,7 +98,14 @@ function generatePassword() {
 
     var specialCharactersPrompt = confirm("Do you want to include special characters?");
 
+    generateCriteria(lowercasePrompt, uppercasePrompt, numericPrompt, specialCharactersPrompt);
 
+
+
+}
+
+
+function generateCriteria(lowercasePrompt, uppercasePrompt, numericPrompt, specialCharactersPrompt){
     //computes the length of the number of criteria selected.\
     //there are 4 criteria, it will decrease by 1 if 'cancel' button is clicked. 
     //an array 'criteria array' is created, and if true, the array's length will increase, and the the corresponding function call will be added on the array index
@@ -133,4 +140,3 @@ function generatePassword() {
 
     }
 }
-
