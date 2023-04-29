8033ab3ab6c14f1ad9d1775aec973ff78969a6d6
Carlos Dela Cruz <cdelacruzzin18@gmail.com>
2023-04-29 13:36:36 -0400

    added criteriaPrompts function

--- script.js
+++ script.js
@@ -9,10 +9,10 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-    var password = prompts();
+    var a = prompts();
     var passwordText = document.querySelector("#password");
 
-    passwordText.value = password;
+    passwordText.value = a;
 
 
 }
@@ -69,15 +69,11 @@ function uppercase() {
     return lowercase().toUpperCase();
 }
 
-
-
-
-
-
-
-
-
-
+//made these global so it can be accessed by other functions
+var lowercasePrompt = true;
+var uppercasePrompt = true;
+var numericPrompt = true;
+var specialCharactersPrompt = true;
 
 function prompts() {
     let criteria = confirm("Here is a list of criteria to include in the password: \n -Length\n-Character Types\n\nPress OK to confim");
@@ -94,12 +90,14 @@ function prompts() {
 
 
     //prompts wheter to include lowercase, uppercase, numbers, or special characters in password
-    var lowercasePrompt = confirm("Do you want to include lowercases?");
-    var uppercasePrompt = confirm("Do you want to include uppercase?");
-    var numericPrompt = confirm("Do you want to include numeric?");
-    var specialCharactersPrompt = confirm("Do you want to include special characters?");
+    lowercasePrompt = confirm("Do you want to include lowercases?");
+    uppercasePrompt = confirm("Do you want to include uppercase?");
+    numericPrompt = confirm("Do you want to include numeric?");
+    specialCharactersPrompt = confirm("Do you want to include special characters?");
 
+    //calls the generateCriteria function
     generateCriteria(lowercasePrompt, uppercasePrompt, numericPrompt, specialCharactersPrompt);
+
 
 
 
@@ -109,13 +107,9 @@ function prompts() {
 //there are 4 criteria, it will decrease by 1 if 'cancel' button is clicked. 
 //an array 'criteria array' is created, and if true, the array's length will increase, and the the corresponding function call will be added on the array index
 //creates a criteriacount variable to be global so it can be accessible by other functions
-// calls the generatePass function, and passes the criteriaArray to it.
-
 
 var criteriaCount = 4;
 function generateCriteria(lowercasePrompt, uppercasePrompt, numericPrompt, specialCharactersPrompt) {
-
-
     var criteriaArray = [];
 
     if (lowercasePrompt == false) {
@@ -123,7 +117,6 @@ function generateCriteria(lowercasePrompt, uppercasePrompt, numericPrompt, speci
     } else {
         criteriaArray.push(lowercase());
     }
-
     if (uppercasePrompt == false) {
         criteriaCount--;
     } else {
@@ -134,29 +127,35 @@ function generateCriteria(lowercasePrompt, uppercasePrompt, numericPrompt, speci
     } else {
         criteriaArray.push(randomInt());
     }
-
     if (specialCharactersPrompt == false) {
         criteriaCount--;
     } else {
         criteriaArray.push(specialChar());
     }
+    for (var a = 0; a < criteriaCount; a++) {
+        console.log("lase: ", criteriaArray[a]);
+    }
 
-   
     randomizedCharacters(criteriaArray);
-      generatePass(passwordLength);
 }
 
 
-function randomizedCharacters(criteriaArray){
-    var randomNum =  Math.floor(Math.random() * criteriaArray.length);
-    console.log('char: ', criteriaArray[randomNum]);
-    
-    return criteriaArray[randomNum]
+function randomizedCharacters(criteriaArray) {
+    var randomNum = Math.floor(Math.random() * criteriaCount)
+    var password = "";
+
+    password = criteriaArray[randomNum];
+    console.log("random char: ", password);
+    generatePass(passwordLength);
+    return x;
 }
 
 function generatePass(passwordLength) {
     var password = "";
+    console.log(passwordLength);
     for (var a = 0; a < passwordLength; a++) {
+        generateCriteria(lowercasePrompt, uppercasePrompt, numericPrompt, specialCharactersPrompt);
         password += randomizedCharacters();
+        console.log(passwordLength);
     }
 }
